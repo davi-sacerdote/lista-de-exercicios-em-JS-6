5cf105439086e970b2aae71419198d0cac4872c1
Davi <rm103828@estudante.fieb.edu.br>
2026-09-17 20:12:13 -0300

    arrumei o E2

--- L6_E2/for.js
+++ L6_E2/for.js
@@ -1,21 +1,31 @@
-// 2) Refaça o programa anterior, mas agora pergunte ao usuário quantos 
-// números ele deseja inserir no array. Depois, o usuário vai preenchendo 
-// elemento por elemento do vetor. Ao final, exiba o array e o array ao 
+// 2) Refaça o programa anterior, mas agora pergunte ao usuário quantos
+// números ele deseja inserir no array. Depois, o usuário vai preenchendo
+// elemento por elemento do vetor. Ao final, exiba o array e o array ao
 // contrário.
 
 const rl = require("readline-sync");
 
 let quantidade = parseInt(rl.question("Quantos numeros você deseja inserir?"));
-let contagem = []
 
+let contagem = [];
+let contrario = [];
 
-for (let n = 0; n<quantidade; n++) {
-    contagem [n] = parseInt(rl.question("Digite um numero: ")); 
+console.log(`Digite ${quantidade} numeros:`);
+
+for (let n = 0; n < quantidade; n++) {
+
+    contagem[n] = parseInt(rl.question("Digite um numero: "));
+
 }
+
+for (let n = quantidade - 1; n >= 0; n--) {
+
+    contrario[quantidade - 1 - n] = contagem[n];
+
+}
+
 console.log(`Array original: `);
-console.table(contagem)
 
-let auxiliar = []
-for (let n = quantidade - 1; n>=0; n--) {
-    console.log(`Array ao contrario: ${contagem[n]}`)
-}
+console.table(contagem);
+
+console.log(`Array ao contrario: ${contrario}`);
